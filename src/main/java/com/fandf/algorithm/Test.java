34d184aece78ae5cf4093d40bdc149676b5b774a
范东风 <365283959@qq.com>
2023-03-10 13:53:05 +0800

    创建项目

---
+++ src/main/java/com/fandf/algorithm/Test.java
@@ -0,0 +1,13 @@
+package com.fandf.algorithm;
+
+/**
+ * @author fandongfeng
+ * @date 2023-3-11 12:58
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+
+}
